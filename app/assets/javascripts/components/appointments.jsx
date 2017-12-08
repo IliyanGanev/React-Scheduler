f64e61c311e77e635f08cbcfec5936933ee82a03
Iliyan Ganev <ganeviliyan@gmail.com>
2017-12-07 21:21:52 -0800

    Set state

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,9 +1,23 @@
 var Appointments = createReactClass({
+	getInitialState: function() {
+		return {
+			appointments: this.props.appointments,
+			input_title: 'Standup Meeting',
+			input_appt_time: 'Tomorrow at 9am'
+		}
+	},
+
+	handleUserInput: function(obj) {
+		this.setState(obj);
+	},
+
 	render: function(){
 		return (
 			<div>
-				<AppointmentForm />
-				<AppointmentsList appointments={this.props.appointments}/>
+				<AppointmentForm input_title={this.state.input_title}  
+					input_appt_time={this.state.input_appt_time}
+					onUserInput={this.handleUserInput} />
+				<AppointmentsList appointments={this.state.appointments}/>
 			</div>
 		)
 	}
