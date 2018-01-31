741e444656db8c9a48d4a54ae09b3bf4b7294380
Iliyan Ganev <ganeviliyan@gmail.com>
2018-01-31 13:31:10 -0800

    asdf

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -26,7 +26,7 @@ class AppointmentForm extends React.Component {
     return (
       <div className='newappointmentdiv'>
         <h2>make new appointment</h2>
-        <form autocomplete="on" onSubmit={(event) => this.handleSubmit(event)}>
+        <form autoComplete="on" onSubmit={(event) => this.handleSubmit(event)}>
           <input size="40" name='title' placeholder='Appointment Title'
             value={this.props.title}
             onChange={(event) => this.handleChange(event)} />
